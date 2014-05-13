9db26ac5b778c976930f48809aa00784264e664c
Kelly Hauenstein <howie@howie23.org>
2014-05-12 23:58:36 -0700

    Add auditions custom taxonomy page
    Add auditions custom taxonomy page
    TODO: Add auditions specific information

---
+++ wp-content/themes/foundation/taxonomy-season-auditions.php
@@ -0,0 +1,10 @@
+<?php
+get_header();
+?>
+    <div class="container">
+        <div id="content">
+        
+        </div><?php //content ?>
+        <?php get_sidebar(); ?>
+    </div><?php //contentContainer ?>
+<?php get_footer(); ?>
