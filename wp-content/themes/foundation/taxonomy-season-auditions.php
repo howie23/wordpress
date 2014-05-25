b50b4725627c9e03a726b352fdf33ef7e962e2d4
Kelly Hauenstein <howie@howie23.org>
2014-05-25 01:58:57 -0700

    Add display of audition information
    Add list of characters and their descriptions and add display for
    multiple directors

--- wp-content/themes/foundation/taxonomy-season-auditions.php
+++ wp-content/themes/foundation/taxonomy-season-auditions.php
@@ -14,23 +14,61 @@
                 <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
                 <hr />
                 <?php the_excerpt(); ?>
+                <p>Directed by 
                 <?php
-                    if (have_rows(audition_dates)) {
-                        $audition_locale = (get_field(audition_locale)) ? get_field(audition_locale) : NULL;
-                        $audition_dates = get_field(audition_dates);
-                        echo '<p>';
-                        echo 'Location: ' . $audition_locale . '<br>';
-                        echo '<ul>';
-                        echo 'Audition Dates:';
-                        foreach ($audition_dates as $dates) {
-                            echo '<li>' . date("l F d, Y", strtotime($dates['audition_date'])) . ' at ' . $dates['audition_time'] . '</li>';
+                    $directors = get_field('show_director');
+                    if ($directors) {
+                        $i = 1;
+                        while (has_sub_field('show_director')) {
+                            $director = get_sub_field('director_name');
+                            if ($i == 1) {
+                                echo $director;
+                            } else {
+                                echo ' and ' . $director;
+                            }
+                            $i++;
                         }
-                        echo '</ul>';
-                        echo '</p>';
-                    } else {
-                        echo '<p><em>Audition dates for ' . get_the_title($post->post_ID) , ' have not been announced.</em></p>';
                     }
                 ?>
+                </p>
+                <?php
+                    $all_audition_information = TRUE; //Show audition information on this page
+                    $audition_dates = get_field(audition_dates); //Get Audition dates/times return an array
+                    $audition_locale = get_field(audition_locale); //Get Audition location
+                    $characters = get_field(cast_members); //Get list of characters and descriptions
+                    //Check to see if at least one Audition date/time has been set set. If not, don't display audition information
+                    if (empty($audition_dates[0]['audition_time']) || empty($audition_dates[0]['audition_date'])) {
+                        $all_audition_information = FALSE;
+                    }
+                    //Check to see if there is an audition location set. If not, don't display audition information
+                    if (empty($audition_locale)) {
+                        $all_audition_information = FALSE;
+                    }
+                    //Check to see if at least one character name and description has been set. If not, don't display audition information
+                    if (empty($characters[0]['character_name']) || empty($characters[0]['character_description'])) {
+                        $all_audition_information = FALSE;
+                    }
+                    if ($all_audition_information)  { ?>
+                        <h3>Audition Information</h3>
+                        <p>
+                            <strong>Location:</strong> <?php echo $audition_locale; ?><br>
+                            <strong>Audition Dates:</strong><br>
+                            <?php
+                            foreach ($audition_dates as $dates) {
+                                echo ' - ' . date("l F d, Y", strtotime($dates['audition_date'])) . ' at ' . $dates['audition_time'] . '<br>';
+                            } ?>
+                        </p>
+                        <h3>Available Roles</h3>
+                        <p>
+                        <?php
+                            foreach ($characters as $character_info) {
+                                echo '<strong>' . $character_info['character_name'] . '</strong> - <em>' . $character_info['character_description'] . '</em><br>';
+                            }
+                        ?>
+                        </p>
+                    <?php } else { ?>
+                        <p><em>Audition information for <?php echo get_the_title($post->post_ID); ?> have not been announced.</em></p>
+                    <?php } ?>
             </div><?php //post ?>
             <?php wp_link_pages(); ?>
         <?php endwhile; else: ?>
