1b7b1c110c767f420597dea90d7a33c2c4565b9e
Kelly Hauenstein <howie@howie23.org>
2014-05-26 22:25:54 -0700

    Code clean-up
    Use CDN for jquery instead of local copy
    Add JS options for Foundation Orbit
    Initialize Foundation Topbar

--- wp-content/themes/foundation/footer.php
+++ wp-content/themes/foundation/footer.php
@@ -1,4 +1,3 @@
-    </div><?php //main body container ?>
     <footer>
         <div class="container">
             <div class="footerDiv">
@@ -16,12 +15,16 @@
         </div>
     </footer>
     <p id="copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-    <script src="<?php echo get_template_directory_uri(); ?>/javascripts/vendor/jquery.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/javascripts/foundation/foundation.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/javascripts/foundation/foundation.orbit.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/javascripts/foundation/foundation.topbar.js"></script>
     <script>
-        $(document).foundation();
+        $(document).foundation('orbit', {
+            resume_on_mouseout: true,
+            slide_number: false,
+        });
+        $(document).foundation('topbar');
     </script>
     <?php wp_footer(); ?>
 </body>
